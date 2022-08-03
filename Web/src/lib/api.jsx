a6541b6b0b7afba34e1dc114023755eb6acdec2d
Gabriel Silva <yzgabrieel02@gmail.com>
2022-08-03 16:13:05 -0300

    MyPortfólio | @degabrielofi_

--- Web/src/lib/api.jsx
+++ Web/src/lib/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://myportfolio-production.up.railway.app/"
+    baseURL: "https://localhosto:5000"
 })
 
 export default api
