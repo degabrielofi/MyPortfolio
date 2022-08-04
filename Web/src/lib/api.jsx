143a2381029308c860866454f35311c745711804
Gabriel Silva <yzgabrieel02@gmail.com>
2022-08-04 13:50:12 -0300

    MyPortfólio | @degabrielofi_

--- Web/src/lib/api.jsx
+++ Web/src/lib/api.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
+const PORT = 'https://myportfolio-production.up.railway.app/'
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_SECRET_API 
+    baseURL: process.env.REACT_APP_SECRET_API || PORT
 })
 
 export default api
