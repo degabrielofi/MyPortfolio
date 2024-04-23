a1629ffde33810dab16dc73a2702080b4e7cfce4
Gabriel Silva <yzgabrieel02@gmail.com>
2024-04-23 01:27:16 -0300

    update `index.html`

--- Web/src/components/Curriculum/index.jsx
+++ Web/src/components/Curriculum/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Button, Socials } from "./style";
-import DeGabrielDEVCurriculum from "assets/DeGabrielDEV CV.pdf";
+import DeGabrielDEVCurriculum from "assets/DeGabrielDEV_CV.pdf";
 
 const Curriculum = () => {
   return (
@@ -9,7 +9,7 @@ const Curriculum = () => {
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
           <path
             fill="#880c85"
-            fill-opacity="1"
+            fillOpacity="1"
             d="M0,64L17.1,90.7C34.3,117,69,171,103,160C137.1,149,171,75,206,53.3C240,32,274,64,309,101.3C342.9,139,377,181,411,181.3C445.7,181,480,139,514,149.3C548.6,160,583,224,617,234.7C651.4,245,686,203,720,192C754.3,181,789,203,823,186.7C857.1,171,891,117,926,128C960,139,994,213,1029,250.7C1062.9,288,1097,288,1131,266.7C1165.7,245,1200,203,1234,202.7C1268.6,203,1303,245,1337,234.7C1371.4,224,1406,160,1423,128L1440,96L1440,320L1422.9,320C1405.7,320,1371,320,1337,320C1302.9,320,1269,320,1234,320C1200,320,1166,320,1131,320C1097.1,320,1063,320,1029,320C994.3,320,960,320,926,320C891.4,320,857,320,823,320C788.6,320,754,320,720,320C685.7,320,651,320,617,320C582.9,320,549,320,514,320C480,320,446,320,411,320C377.1,320,343,320,309,320C274.3,320,240,320,206,320C171.4,320,137,320,103,320C68.6,320,34,320,17,320L0,320Z"
           ></path>
         </svg>
@@ -21,10 +21,7 @@ const Curriculum = () => {
               currículo completo abaixo.
             </p>
             <Button>
-              <a
-                href={DeGabrielDEVCurriculum}
-                download={DeGabrielDEVCurriculum}
-              >
+              <a href={DeGabrielDEVCurriculum} download="DeGabrielDEV CV.pdf">
                 <button type="button">
                   <div className="svg-wrapper-1">
                     <div className="svg-wrapper">
@@ -102,13 +99,13 @@ const Curriculum = () => {
                   gradientUnits="userSpaceOnUse"
                   transform="matrix(1, 0, 0, 1, -7.105427357601002e-15, -3.552713678800501e-15)"
                 >
-                  <stop offset="0" stop-color="#fd5"></stop>
-                  <stop offset=".328" stop-color="#ff543f"></stop>
-                  <stop offset=".348" stop-color="#fc5245"></stop>
-                  <stop offset=".504" stop-color="#e64771"></stop>
-                  <stop offset=".643" stop-color="#d53e91"></stop>
-                  <stop offset=".761" stop-color="#cc39a4"></stop>
-                  <stop offset=".841" stop-color="#c837ab"></stop>
+                  <stop offset="0" stopColor="#fd5"></stop>
+                  <stop offset=".328" stopColor="#ff543f"></stop>
+                  <stop offset=".348" stopColor="#fc5245"></stop>
+                  <stop offset=".504" stopColor="#e64771"></stop>
+                  <stop offset=".643" stopColor="#d53e91"></stop>
+                  <stop offset=".761" stopColor="#cc39a4"></stop>
+                  <stop offset=".841" stopColor="#c837ab"></stop>
                 </radialGradient>
                 <path
                   fill="url(#yOrnnhliCrdS2gy~4tD8ma_Xy10Jcu1L2Su_gr1)"
@@ -122,11 +119,11 @@ const Curriculum = () => {
                   gradientTransform="matrix(1 0 0 .6663 0 1.849)"
                   gradientUnits="userSpaceOnUse"
                 >
-                  <stop offset="0" stop-color="#4168c9"></stop>
+                  <stop offset="0" stopColor="#4168c9"></stop>
                   <stop
                     offset=".999"
-                    stop-color="#4168c9"
-                    stop-opacity="0"
+                    stopColor="#4168c9"
+                    stopOpacity="0"
                   ></stop>
                 </radialGradient>
                 <path
